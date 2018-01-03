a21d4104dfa4c5226fb817874134dd264adf86c3
fomenyesu <fomenyesu@gmail.com>
2018-01-03 13:52:27 +0800

    update dva2.0
    update dva2.0

--- static/src/utils/config.js
+++ static/src/utils/config.js
@@ -2,8 +2,13 @@ module.exports = {
   name: 'Restapi Module',
   prefix: 'fomenyesu',
   footerText: 'Restapi Module 版权所有 © 2017 由 fomenyesu 支持',
+  logoSrc: 'http://127.0.0.1:7001/public/assets/logo.png',
   // logoSrc: './assets/logo.png',
   logoText: 'Restapi Module',
   needLogin: true,
+  iconFontUrl: 'http://127.0.0.1:7001/public/assets/znfont.js',
   // iconFontUrl: './assets/znfont.js',
+  devBaseURL: 'http://127.0.0.1:7001',
+  prdBaseURL: 'http://127.0.0.1:7001',
+  attachmentURL: 'http://127.0.0.1:7001/public/uploads'
 }
